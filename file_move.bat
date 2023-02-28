cb9c1353eadd1f45d02635014a19fc6abc8632d2
HTR2106 <50503701+tyurome@users.noreply.github.com>
2023-02-28 10:38:16 +0900

    add delete fnc

--- file_move.bat
+++ file_move.bat
@@ -4,4 +4,7 @@ set search=*.mp4 *.avi *.wmv
 
 for /f "tokens=*" %%a in ('dir /b /s *.mp4 *.avi *.wmv') do (
     move "%%a" %dest%
+    for %%b in ("%%~dpa.") do (
+        rd /s /q "%%~fb"
+  )
 )
